c687cd16f33c3297bd149f6b83bf51387240ded3
Timothée Peignier <timothee.peignier@tryphon.org>
2013-07-27 11:22:38 -0700

    bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if "java" not in version and "pypy" not in version:
 
 setup(
     name='atomic',
-    version='0.5.3',
+    version='0.6',
     description='An atomic class that guarantees atomic updates to its contained value.',
     long_description=readme,
     author='Timothée Peignier',
