e5fd4dda7cb2eb3a1a6cce748c21f905b079068b
Timothée Peignier <timothee.peignier@tryphon.org>
2014-03-15 21:14:42 -0700

    use cffi and __atomic gcc built-ins

--- atomic/__init__.py
+++ atomic/__init__.py
@@ -1,45 +1,72 @@
-__all__ = ["Atomic", "ConcurrentUpdateException"]
+from cffi import FFI
+
+ffi = FFI()
+
+ffi.cdef("""
+void long_store(long *, long *);
+long long_add_and_fetch(long *, long);
+long long_sub_and_fetch(long *, long);
+long long_get_and_set(long *, long);
+long long_compare_and_set(long *, long *, long);
+""")
+
+atomic = ffi.verify("""
+void long_store(long *v, long *n) {
+    __atomic_store(v, n, __ATOMIC_SEQ_CST);
+};
+long long_add_and_fetch(long *v, long i) {
+    return __atomic_add_fetch(v, i, __ATOMIC_SEQ_CST);
+};
+long long_sub_and_fetch(long *v, long i) {
+    return __atomic_sub_fetch(v, i, __ATOMIC_SEQ_CST);
+};
+long long_get_and_set(long *v, long n) {
+    return __atomic_exchange_n(v, n, __ATOMIC_SEQ_CST);
+};
+long long_compare_and_set(long *v, long *e, long n) {
+    return __atomic_compare_exchange_n(v, e, n, 0, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+};
+""")
 
 
-try:
-    try:
-        from com.lapanthere.atomic import Reference
-    except ImportError:
-        from atomic._reference import Reference  # noqa
-except ImportError:
-    from atomic.reference import Reference  # noqa
-
-
-class ConcurrentUpdateException(Exception):
-    pass
-
-
-class Atomic(object):
+class AtomicLong(object):
     """
-    An atomic class that guarantees atomic updates to its contained value.
+    An atomic class that guarantees atomic updates to its contained integer value.
     """
     def __init__(self, value=None):
         """
-        Creates a new Atomic with the given initial value.
+        Creates a new AtomicLong with the given initial value.
 
         :param value: initial value
         """
-        self.ref = Reference(value)
+        self._value = ffi.new('long *', value)
 
-    def get_value(self):
-        return self.ref.get()
+    def __repr__(self):
+        return '<{0} at 0x{1:x}: {2!r}>'.format(
+            self.__class__.__name__, id(self), self.value)
 
-    def set_value(self, value):
-        return self.ref.set(value)
+    @property
+    def value(self):
+        return self._value[0]
 
-    value = property(get_value, set_value, doc="""Get or set current value""")
+    @value.setter
+    def value(self, new):
+        atomic.long_store(self._value, ffi.new('long *', new))
+
+    def __iadd__(self, inc):
+        atomic.long_add_and_fetch(self._value, inc)
+        return self
+
+    def __isub__(self, dec):
+        atomic.long_sub_and_fetch(self._value, dec)
+        return self
 
     def get_and_set(self, new_value):
         """Atomically sets to the given value and returns the old value
 
         :param new_value: the new value
         """
-        return self.ref.get_and_set(new_value)
+        return atomic.long_get_and_set(self._value, new_value)
 
     def swap(self, new_value):
         return self.get_and_set(new_value)
@@ -52,43 +79,44 @@ class Atomic(object):
         :param expect_value: the expected value
         :param new_value: the new value
         """
-        return self.ref.compare_and_set(expect_value, new_value)
+        return bool(atomic.long_compare_and_set(self._value, ffi.new('long *', expect_value), new_value))
 
     def compare_and_swap(self, expect_value, new_value):
         return self.compare_and_set(expect_value, new_value)
 
-    def update(self, update_func):
+
+class AtomicLongArray(object):
+    """
+    An atomic class that guarantees atomic updates to its contained integer values.
+    """
+    def __init__(self, array=[]):
         """
-        Update value based on the given function.
+        Creates a new AtomicLongArray with the given initial array of integers.
 
-        It may run the block repeatedly if there are other concurrent
-        updates in progress.
-
-        :param update_func: a function that given the current value
-            return the new value
-        :type update_func: func
+        :param array: initial values
         """
-        update = False
-        while not update:
-            old_value = self.ref.get()
-            new_value = update_func(old_value)
-            update = self.ref.compare_and_set(old_value, new_value)
-        return new_value
+        self._array = [AtomicLong(x) for x in array]
 
-    def try_update(self, update_func):
-        """
-        Update value based on the given function.
+    def __len__(self):
+        return len(self._array)
 
-        If the value changes before the update can happen,
-        it will raise a :class:`ConcurrentUpdateException`.
+    def __getitem__(self, key):
+        return self._array[key].value
 
-        :param update_func: a function that given the current
-            value return the new value
-        :type update_func: func
-        :raises: ConcurrentUpdateException
-        """
-        old_value = self.ref.get()
-        new_value = update_func(old_value)
-        while not self.ref.compare_and_set(old_value, new_value):
-            raise ConcurrentUpdateException("Updating reference failed")
-        return new_value
+    def __setitem__(self, key, value):
+        if self._array[key]:
+            self._array[key].value = value
+        else:
+            self._array[key] = AtomicLong(value)
+
+    def __iter__(self):
+        for a in self._array:
+            yield a.value
+
+    @property
+    def value(self):
+        return [a.value for a in self._array]
+
+    @value.setter
+    def value(self, new=[]):
+        self._array = [AtomicLong(int(x)) for x in new]
--- tests/test_atomic.py
+++ tests/test_atomic.py
@@ -1,106 +1,74 @@
-import sys
-
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest  # noqa
 
-from atomic import Atomic, ConcurrentUpdateException
-from atomic.reference import Reference as py_reference
-
-try:
-    import atomic._reference
-except ImportError as e:
-    c_reference = None
-else:
-    c_reference = atomic._reference.Reference
-
-is_jython = sys.platform.startswith("java")
-if is_jython:
-    from com.lapanthere.atomic import Reference as java_reference
-else:
-    java_reference = None  # noqa
+from atomic import AtomicLong, AtomicLongArray
 
 
-class AtomicTest(object):
-    def setUp(self):
-        reference_cls = self._reference
-
-        class MockAtomic(Atomic):
-            def __init__(self, value=None):
-                self.ref = reference_cls(value)
-        self.atomic_cls = MockAtomic
-
+class AtomicLongTest(unittest.TestCase):
     def test_init(self):
-        atomic = self.atomic_cls()
-        self.assertEqual(None, atomic.value)
+        atomic = AtomicLong()
+        self.assertEqual(0, atomic.value)
 
-        atomic = self.atomic_cls(0)
+        atomic = AtomicLong(0)
         self.assertEqual(0, atomic.value)
 
     def test_value(self):
-        atomic = self.atomic_cls(0)
+        atomic = AtomicLong(0)
         atomic.value = 1
         self.assertEqual(1, atomic.value)
 
     def test_swap(self):
-        atomic = self.atomic_cls(1000)
+        atomic = AtomicLong(1000)
         swapped = atomic.swap(1001)
         self.assertEqual(1001, atomic.value)
         self.assertEqual(1000, swapped)
 
-    def test_update(self):
-        atomic = self.atomic_cls(1000)
-        value = atomic.update(lambda v: v + 1)
+    def test_compare_and_swap(self):
+        atomic = AtomicLong(1000)
+        swapped = atomic.compare_and_swap(1000, 1001)
         self.assertEqual(1001, atomic.value)
-        self.assertEqual(1001, value)
+        self.assertEqual(True, swapped)
 
-    def test_try_update(self):
-        atomic = self.atomic_cls(1000)
-        value = atomic.try_update(lambda v: v + 1)
+        swapped = atomic.compare_and_swap(1000, 1024)
         self.assertEqual(1001, atomic.value)
-        self.assertEqual(1001, value)
-        reference = atomic.ref
+        self.assertEqual(False, swapped)
 
-        class MockReference(object):
-            def get(self):
-                return reference.get()
+    def test_add(self):
+        atomic = AtomicLong(1000)
+        atomic += 1
+        self.assertEqual(1001, atomic.value)
 
-            def compare_and_set(self, old_value, new_value):
-                return False
+    def test_sub(self):
+        atomic = AtomicLong(1000)
+        atomic -= 1
+        self.assertEqual(999, atomic.value)
 
-        atomic.ref = MockReference()
-        with self.assertRaises(ConcurrentUpdateException):
-            atomic.try_update(lambda v: v + 1)
+class AtomicLongArrayTest(unittest.TestCase):
+    def test_init(self):
+        atomic = AtomicLongArray()
+        self.assertEqual([], atomic.value)
 
-    def test_complex_value(self):
-        atomic = self.atomic_cls([-1, 0])
+        atomic = AtomicLongArray([-1, 0])
         self.assertEqual([-1, 0], atomic.value)
 
-    def test_complex_update(self):
-        def complex_update(v):
-            return [v + 1 for v in v]
-        atomic = self.atomic_cls([-1, 0])
-        value = atomic.update(complex_update)
-        self.assertEqual([0, 1], atomic.value)
-        self.assertEqual([0, 1], value)
+    def test_value(self):
+        atomic = AtomicLongArray([])
+        atomic.value = [-1, 0]
+        self.assertEqual([-1, 0], atomic.value)
 
+    def test_get(self):
+        atomic = AtomicLongArray([-1, 0])
+        self.assertEqual(atomic[0], -1)
+        self.assertEqual(atomic[1], 0)
 
-class PyAtomicTest(AtomicTest, unittest.TestCase):
-    _reference = py_reference
+    def test_set(self):
+        atomic = AtomicLongArray([-1, 0])
+        atomic[1] = -2
+        self.assertEqual(atomic[1], -2)
 
-
-@unittest.skipUnless(c_reference, "requires the C _reference module")
-class CAtomicTest(AtomicTest, unittest.TestCase):
-    _reference = c_reference
-
-    def test_loaded(self):
-        self.assertIsInstance(Atomic().ref, self._reference)
-
-
-@unittest.skipUnless(is_jython, "requires Jython")
-class JythonAtomicTest(AtomicTest, unittest.TestCase):
-    _reference = java_reference
-
-    def test_loaded(self):
-        self.assertIsInstance(Atomic().ref, self._reference)
+    def test_iter(self):
+        atomic = AtomicLongArray([-1, 0])
+        for i, a in enumerate(atomic):
+            self.assertEqual(atomic[i], a)
